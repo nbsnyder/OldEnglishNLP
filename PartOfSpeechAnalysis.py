e872346d03cf3df97007691d23f3976c994b8b56
Nathan Snyder <nathan.snyder13@gmail.com>
2021-12-14 02:11:29 -0500

    improved the model used in the part of speech analysis

--- PartOfSpeechAnalysis.py
+++ PartOfSpeechAnalysis.py
@@ -25,7 +25,10 @@ cursor.close()
 connection.close()
 
 # Remove any entries that are missing a headword or part of speech
-data = [x for x in data if x[0] != None and x[1] != None]
+data = [x for x in data if x[0] != None and x[1] != None and x[0] != "" and x[1] != ""]
+
+# Remove unwanted characters from the spellings
+data = [(Utils.removeUnwantedCharsFromWord(x[0]), Utils.removeUnwantedCharsFromWord(x[1])) for x in data]
 
 # Randomize the order of the data entries
 random.shuffle(data)
@@ -54,6 +57,18 @@ for x in partsOfSpeech:
 partsOfSpeech = [partOfSpeechToInt[x] for x in partsOfSpeech]
 partsOfSpeech = np.array(partsOfSpeech, dtype=np.uint8)
 
+partsOfSpeechArrForm = [[0] * numPartsOfSpeech] * len(partsOfSpeech)
+for i in range(len(partsOfSpeech)):
+    partsOfSpeechArrForm[i][partsOfSpeech[i]] = 1
+partsOfSpeechArrForm = np.array(partsOfSpeechArrForm, dtype=np.uint8)
+
+def partsOfSpeechArrFormToInt(arr):
+    maxVal = np.amax(arr)
+    for i in range(len(arr)):
+        if np.abs(arr[i] - maxVal) < 0.000001:
+            return i
+    return -1
+
 # Tranform each headword into a character array of length `maxWordLength`
 # The headword is padded with \0 if the headword is not already of length `maxWordLength`
 headwords = [Utils.wordToCharArray(x, maxWordLength) for x in headwords]
@@ -63,25 +78,59 @@ headwords = np.array(headwords, dtype=np.float64)
 maxCharValue = np.amax(headwords)
 headwords = headwords / maxCharValue
 
+
 # Split data into training (80%), validation (10%), and testing (10%) sets
 trainingSetCutoff = int(len(data) * 0.8)
 validationSetCutoff = int(len(data) * 0.9)
+
 headwordsTrainingSet = headwords[:trainingSetCutoff, :]
-partsOfSpeechTrainingSet = partsOfSpeech[:trainingSetCutoff]
+partsOfSpeechArrFormTrainingSet = partsOfSpeechArrForm[:trainingSetCutoff, :]
+
 headwordsValidationSet = headwords[trainingSetCutoff:validationSetCutoff, :]
-partsOfSpeechValidationSet = partsOfSpeech[trainingSetCutoff:validationSetCutoff]
+partsOfSpeechArrFormValidationSet = partsOfSpeechArrForm[trainingSetCutoff:validationSetCutoff, :]
+
 headwordsTestingSet = headwords[validationSetCutoff:, :]
+partsOfSpeechArrFormTestingSet = partsOfSpeechArrForm[validationSetCutoff:, :]
 partsOfSpeechTestingSet = partsOfSpeech[validationSetCutoff:]
 
+
 # Make the model
-inputs = keras.Input(shape=(maxWordLength))
-x = layers.Dense(maxWordLength, activation=activations.tanh)(inputs)
-outputs = layers.Dense(1, activation=activations.softmax)(x)
-model = keras.Model(inputs=inputs, outputs=outputs)
+model = keras.Sequential([
+    keras.layers.Dense(maxWordLength, activation='relu'),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(numPartsOfSpeech)
+])
 
 # Compile and plot the model
-model.compile(optimizer=keras.optimizers.RMSprop(learning_rate=1e-3), loss=keras.losses.CategoricalCrossentropy())
-plot_model(model, to_file="ModelPlots/PartOfSpeechAnalysisModel.png", show_shapes=True, show_layer_names=True)
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=tf.keras.losses.CategoricalCrossentropy(),
+)
 
 # Fit the data
-model.fit(headwordsTrainingSet, partsOfSpeechTrainingSet, batch_size=32, epochs=10)
+model.fit(
+    headwordsTrainingSet, 
+    partsOfSpeechArrFormTrainingSet, 
+    validation_data=(headwordsValidationSet, partsOfSpeechArrFormValidationSet), 
+    batch_size=16, 
+    epochs=25
+)
+
+# Plot the model
+plot_model(
+    model, 
+    to_file="ModelPlots/PartOfSpeechAnalysisModel.png", 
+    show_shapes=True, 
+    show_layer_names=True
+)
+
+# Evaluate the model
+modelOutputArrForm = model.call(tf.convert_to_tensor(headwordsTestingSet), training=False).numpy()
+modelOutput = [partsOfSpeechArrFormToInt(x) for x in modelOutputArrForm]
+
+accuracy = 0
+for i in range(len(headwordsTestingSet)):
+    if (partsOfSpeechTestingSet[i] == modelOutput[i]):
+        accuracy += 1
+accuracy /= len(headwordsTestingSet)
+print("\n\nAccuracy: %.3f%%" % (accuracy * 100))
